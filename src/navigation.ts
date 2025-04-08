e501cbad99f2237d5b034c51f24dfa8a57ab29e0
Dann <daniel.villarruel@yachaytech.edu.ec>
2025-04-08 12:07:08 -0500

    update

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,6 +3,18 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
+      text: 'Olimpiada Nacional',
+      href: '#',
+    },
+    {
+      text: 'Internacional',
+      href: '#',
+    },
+    {
+      text: 'Recursos',
+      href: '#',
+    },
+    {
       text: 'Blog',
       href: '#',
     },
@@ -31,7 +43,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/astro-ec' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="~/src/assets/images/nwlg.png" alt="OEAA logo" loading="lazy"></img>
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="~/assets/images/nwlg.png" alt="OEAA logo" loading="lazy"></img>
     The Ecuadorian Olympiadas on Astronomy and Astrophysics · All rights reserved.
   `,
 };
