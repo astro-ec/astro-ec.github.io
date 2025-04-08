eeb9dbf214efd443b8d7567a28f06578850473c3
Dann <daniel.villarruel@yachaytech.edu.ec>
2025-04-08 12:13:36 -0500

    update

--- src/navigation.ts
+++ src/navigation.ts
@@ -27,9 +27,17 @@ export const footerData = {
     {
       title: 'La Olimpiada Nacional',
       links: [
-        { text: 'Cronograma 8va Olimpiada 2025', href: '#' },
+        { text: 'Cronograma 2025', href: '#' },
         { text: 'Formato', href: '#' },
         { text: 'Inscripción', href: '#' },
+      ],
+    },
+    {
+      title: 'Recursos',
+      links: [
+        { text: 'Exámenes Nacionales', href: '#' },
+        { text: 'Exámenes Internacionales', href: '#' },
+        { text: 'Material de estudio', href: '#' },
       ],
     },
   ],
@@ -39,11 +47,10 @@ export const footerData = {
   ],
   socialLinks: [
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/oeaa_ec' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/oeaaecuado' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/oeaaecuador' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/astro-ec' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="~/assets/images/nwlg.png" alt="OEAA logo" loading="lazy"></img>
-    The Ecuadorian Olympiadas on Astronomy and Astrophysics · All rights reserved.
+    The Ecuadorian Olympiadas on Astronomy and Astrophysics 2025 · All rights reserved.
   `,
 };
